f6a2b032bf44c6d564471a72bb619b45b146a4cc
unknown <wanderson.rocha@oseusonho.com.br>
2018-06-19 17:32:30 -0300

    primeiro commit do projeto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="page-container">
+    <md-app md-mode="hello">
+      <md-app-toolbar class="md-primary">
+        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-title">SGA</span>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible">
+        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>move_to_inbox</md-icon>
+            <span class="md-list-item-text">Inbox</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Sent Mail</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>delete</md-icon>
+            <span class="md-list-item-text">Trash</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>error</md-icon>
+            <span class="md-list-item-text">Spam</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+    <div>
+      <form-login></form-login>
+
+    </div>
+      </md-app-content>
+    </md-app>
+
+  </div>
+</template>
+
+<script>
+import FormLogin from './forms/FormLogin.vue'
+export default {
+  name: "HelloWorld",
+  components: {
+    FormLogin
+  },
+  data: () => ({
+    menuVisible: false
+  })
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+.md-app {
+  border: 1px solid rgba(#000, 0.12);
+}
+.md-drawer {
+  width: 230px;
+  max-width: calc(100vw - 125px);
+}
+
+</style>
