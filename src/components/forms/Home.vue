587a3ebfc24e3ae40b297a29fb9ff942b81793d8
unknown <wanderson.rocha@oseusonho.com.br>
2018-06-25 18:12:45 -0300

    dialog create

--- src/components/forms/Home.vue
+++ src/components/forms/Home.vue
@@ -1,74 +1,123 @@
 <template>
-  <div class="page-container">
-    <md-app md-mode="hello">
-      <md-app-toolbar class="md-toobar">
-        <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+<div class="page-container">
+    <md-app>
+      <md-app-toolbar class="md-toobar" md-elevation="0">
+        <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
           <md-icon>menu</md-icon>
         </md-button>
-        <span class="md-title">SGA</span>
+        <span class="md-title" style="flex: 1">Prosperidade</span>
+
+      <md-button class="md-primary">Sair</md-button>
       </md-app-toolbar>
 
-      <md-app-drawer :md-active.sync="menuVisible">
-        <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+      <md-app-drawer :md-active.sync="menuVisible" md-persistent="mini">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          <span>Menu</span>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button md-dense" @click="toggleMenu">
+              <md-icon>keyboard_arrow_left</md-icon>
+            </md-button>
+          </div>
+        </md-toolbar>
 
         <md-list>
           <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Inbox</span>
+            <md-icon>dashboard</md-icon>
+            <md-button>Dashboard</md-button>
+          </md-list-item>
+          <md-list-item>
+            <md-icon>gavel</md-icon>
+            <md-button>Admin</md-button>
           </md-list-item>
 
           <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
+            <md-icon>contacts</md-icon>
+            <md-button>Clientes</md-button>
           </md-list-item>
 
           <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
+            <md-icon>list</md-icon>
+            <md-button>Listas</md-button>
           </md-list-item>
 
           <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
+            <md-icon>create</md-icon>
+            <md-button>Alterar</md-button>
           </md-list-item>
         </md-list>
       </md-app-drawer>
 
       <md-app-content>
-    <div>
-
-    </div>
+        <list-cliente/>
       </md-app-content>
     </md-app>
+    <md-dialog :md-active.sync="showDialog">
+      <md-dialog-title>Preferences</md-dialog-title>
+      <md-dialog-actions>
+        <md-button class="md-primary" @click="showDialog = false">Close</md-button>
+        <md-button class="md-primary" @click="showDialog = false">Save</md-button>
+      </md-dialog-actions>
+    </md-dialog>
+    <md-speed-dial class="md-bottom-right" md-direction="bottom">
+      <md-speed-dial-target>
+        <md-icon class="md-morph-initial">add</md-icon>
+        <md-icon class="md-morph-final">edit</md-icon>
+      </md-speed-dial-target>
 
+      <md-speed-dial-content>
+        <md-button class="md-icon-button" @click="showDialog = true">
+          <md-icon>contacts</md-icon>
+        </md-button>
+
+        <md-button class="md-icon-button" @click="showDialog = true">
+          <md-icon>event</md-icon>
+        </md-button>
+      </md-speed-dial-content>
+    </md-speed-dial>
   </div>
 </template>
 
 <script>
-import FormAddUser from './FormAddUser'
-import FormLogin from './FormLogin.vue'
+import FormAddUser from "./FormAddUser";
+import FormLogin from "./FormLogin.vue";
+import ListCliente from '../lists/ListCliente';
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   components: {
     FormAddUser,
+    ListCliente
   },
   data: () => ({
     menuVisible: false,
-  })
+    showDialog: false
+  }),
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 .md-app {
+  min-height: 350px;
   border: 1px solid rgba(#000, 0.12);
 }
+
+// Demo purposes only
 .md-drawer {
-  width: 900px;
+  width: 230px;
   max-width: calc(100vw - 125px);
 }
-.md-toobar{
-  background-color:#00FA9A;
+.md-toobar {
+  background-color: #48D1CC;
 }
-
+.md-fab {
+  display: block;
+  float: right;
+  margin-top: 200px;
+}
 </style>
