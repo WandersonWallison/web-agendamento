be3cffc53087986d36fe7a5f7ebb137f13ab0eff
unknown <wanderson.rocha@oseusonho.com.br>
2018-08-15 19:16:37 -0300

    novas function agente

--- src/components/lists/ListAgendamento.vue
+++ src/components/lists/ListAgendamento.vue
@@ -1,97 +1,168 @@
 <template>
-  <div class="conteudo-alinhamento">
-    <md-toolbar md-elevation="0" class="md-dense">
-          <span class="md-title">Confimação de Agendamento</span>
-    </md-toolbar>
-    <md-table v-model="searched" md-sort="name" md-sort-order="asc" md-card md-fixed-header>
+  <div>
+    <md-table v-model="schedules" md-card @md-selected="onSelect">
       <md-table-toolbar>
-        <div class="md-toolbar-section-start">
-          <h1 class="md-title"></h1>
-        </div>
-
-        <md-field md-clearable class="md-toolbar-section-end">
-          <md-input placeholder="Buscar pelo nome..." v-model="search" @input="searchOnTable" />
-        </md-field>
+        <h1 class="md-title">Painel de Agendamento</h1>
       </md-table-toolbar>
-      <!--
-      <md-table-empty-state
-        md-label="No users found"
-        :md-description="`No user found for this '${search}' query. Try a different search term or create a new user.`">
-        <md-button class="md-primary md-raised" @click="newUser">Create New User</md-button>
-      </md-table-empty-state>
-      -->
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="Código" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
-        <md-table-cell md-label="Nome" md-sort-by="data">{{ item.lead.nome }}</md-table-cell>
-        <md-table-cell md-label="Observação" md-sort-by="obs">{{ item.obs }}</md-table-cell>
-        <md-table-cell md-label="Telefone" md-sort-by="title">{{ item.telefone }}</md-table-cell>
-       <md-table-cell md-label="Status" md-sort-by="status">
-          <md-button class="md-icon-button md-raised md-primary">
+
+      <md-table-row slot="md-table-row" slot-scope="{ item }" :class="getClass(item)" md-selectable="single">
+        <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
+        <md-table-cell md-label="Name" md-sort-by="name">{{ item.id_lead.nome }}</md-table-cell>
+        <md-table-cell md-label="Email" md-sort-by="email">{{ item.id_lead.email }}</md-table-cell>
+        <md-table-cell md-label="Telefone" md-sort-by="telefone">{{ item.id_lead.telefone }}</md-table-cell>
+        <md-table-cell md-label="Status" md-sort-by="status">
+          <md-button class="md-icon-button md-raised md-primary" @click="aceito">
             <md-tooltip md-direction="top">Aceito</md-tooltip>
             <md-icon>thumb_up</md-icon>
           </md-button>
-          <md-button class="md-icon-button md-raised md-accent">
+          <md-button class="md-icon-button md-raised md-accent"  @click="notAceito">
             <md-tooltip md-direction="top">Não aceito</md-tooltip>
             <md-icon>thumb_down</md-icon>
           </md-button>
           </md-table-cell>
       </md-table-row>
     </md-table>
+  <div v-if="selected">
+     <div class="viewport">
+      <md-toolbar :md-elevation="1">
+        <span class="md-title">Contato</span>
+      </md-toolbar>
+
+      <md-list class="md-double-line">
+        <md-subheader>Phone</md-subheader>
+
+        <md-list-item>
+          <md-icon class="md-primary">phone</md-icon>
+
+          <div class="md-list-item-text">
+            <span>{{ selected.id_lead.telefone }}</span>
+            <span>Mobile</span>
+          </div>
+
+          <md-button class="md-icon-button md-list-action">
+            <md-icon>sms</md-icon>
+          </md-button>
+        </md-list-item>
+
+        <md-divider></md-divider>
+        <md-subheader>Email</md-subheader>
+
+        <md-list-item>
+          <md-icon class="md-primary">email</md-icon>
+
+          <div class="md-list-item-text">
+            <span>{{ selected.id_lead.email }}</span>
+            <span>Personal</span>
+          </div>
+        </md-list-item>
+
+      </md-list>
+    </div>
+
+    <div class="viewport">
+      <md-toolbar :md-elevation="1">
+        <span class="md-title">Endereço</span>
+      </md-toolbar>
+
+      <md-list>
+        <md-list-item>
+          <md-icon>map</md-icon>
+          <span class="md-list-item-text">{{ selected.endereco[0].logradouro }}</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>map</md-icon>
+          <span class="md-list-item-text">{{ selected.endereco[0].cidade }}</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>map</md-icon>
+          <span class="md-list-item-text">{{ selected.endereco[0].bairro }}</span>
+        </md-list-item>
+
+        <md-list-item>
+          <md-icon>map</md-icon>
+          <span class="md-list-item-text">{{ selected.endereco[0].numero }}</span>
+        </md-list-item>
+        <md-list-item>
+          <md-icon>map</md-icon>
+          <span class="md-list-item-text">{{ selected.endereco[0].estado }}</span>
+        </md-list-item>
+
+      </md-list>
+    </div>
+  </div>
   </div>
 </template>
+
 <script>
 import axios from 'axios'
-const toLower = text => {
-  return text.toString().toLowerCase()
-}
-const searchByName = (items, term) => {
-  if (term) {
-    return items.filter(item => toLower(item.nome).includes(toLower(term)))
-  }
-  return items
-}
-export default {
-  name: 'listAgendamento',
-  data: () => ({
-    search: null,
-    searched: [],
-    schedules: []
-  }),
-  mounted () {
-    axios.get(process.env.API + 'schedule')
+  export default {
+    name: 'ListeAgendamento',
+    data: () => ({
+      selected: {},
+      schedules: []
+    }),
+    mounted () {
+    axios.get(process.env.API + 'schedule?where={"status": 0}')
       .then(response => {
         this.schedules = response.data
-        this.searched = response.data
+        this.selected = null
       })
   },
-  methods: {
-    newUser () {
-      window.alert('Noop')
+    methods: {
+      aceito () {
+      let newAgenda = {
+        status: 1
+      }
+      axios.put(process.env.API + 'schedule/' + this.selected.id, newAgenda)
+        .then((response) => {
+          this.results = response.data
+          alert('Confirmado agendamento')
+          window.location.reload()
+        })
+        .catch((error) => {
+          alert(error.response.data.code)
+          console.log(error.response.data)
+        })
     },
-    searchOnTable () {
-      this.searched = searchByName(this.schedules, this.search)
+    notAceito () {
+      let newAgenda = {
+        status: 2
+      }
+      axios.put(process.env.API + 'schedule/' + this.selected.id, newAgenda)
+        .then((response) => {
+          this.results = response.data
+          alert('Não confirmou agendamento')
+          window.location.reload()
+        })
+        .catch((error) => {
+          alert(error.response.data.code)
+          console.log(error.response.data)
+        })
+    },
+      getClass: ({ id }) => ({
+        'md-primary': id === 2,
+        'md-accent': id === 3
+      }),
+      onSelect (item) {
+        this.selected = item
+      }
     }
-  },
-  created () {
-    this.searched = this.users
   }
-}
 </script>
 
 <style lang="scss" scoped>
-  .md-field {
-    max-width: 300px;
+  .md-table + .md-table {
+    margin-top: 16px
   }
-  .conteudo-alinhamento{
-     text-align: left;
-  }
-  .butoom-02{
-    background-color: green;
-  }
-  .butoom-03{
-    background-color: rgb(220, 235, 13);
-  }
-  .butoom-04{
-    background-color: rgb(235, 102, 13);
+  .viewport {
+    width: 45%;
+    max-width: 100%;
+    display: inline-block;
+    vertical-align: top;
+    overflow: auto;
+    margin-top: 5%;
+    border: 1px solid rgba(#000, .12);
   }
 </style>
