156f562c03e4100e97f45fc524522abc4d819a38
unknown <wanderson.rocha@oseusonho.com.br>
2018-08-16 09:08:22 -0300

    label button

--- src/components/buttons/ButtonAdd.vue
+++ src/components/buttons/ButtonAdd.vue
@@ -7,12 +7,15 @@
       </md-speed-dial-target>
       <md-speed-dial-content>
         <md-button class="md-icon-button" @click="showDialog = true">
+          <md-tooltip md-direction="top">Contato</md-tooltip>
           <md-icon>note</md-icon>
         </md-button>
         <md-button class="md-icon-button" @click="showAgente = true">
+          <md-tooltip md-direction="top">Agente</md-tooltip>
           <md-icon>people</md-icon>
         </md-button>
         <md-button class="md-icon-button" @click="showEmpresa = true">
+          <md-tooltip md-direction="top">Escritório</md-tooltip>
           <md-icon>location_city</md-icon>
         </md-button>
       </md-speed-dial-content>
