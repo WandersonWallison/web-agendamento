12824f3a3952f8a4dc347a902b8613da508f4f2f
Wellington Santos <wellington.m.santos@gmail.com>
2018-08-16 09:11:46 -0300

    alteração da mensagem do botão

--- src/components/buttons/ButtonAdd.vue
+++ src/components/buttons/ButtonAdd.vue
@@ -7,15 +7,15 @@
       </md-speed-dial-target>
       <md-speed-dial-content>
         <md-button class="md-icon-button" @click="showDialog = true">
-          <md-tooltip md-direction="top">Contato</md-tooltip>
+          <md-tooltip md-direction="left">Contato</md-tooltip>
           <md-icon>note</md-icon>
         </md-button>
         <md-button class="md-icon-button" @click="showAgente = true">
-          <md-tooltip md-direction="top">Agente</md-tooltip>
+          <md-tooltip md-direction="left">Agente</md-tooltip>
           <md-icon>people</md-icon>
         </md-button>
         <md-button class="md-icon-button" @click="showEmpresa = true">
-          <md-tooltip md-direction="top">Escritório</md-tooltip>
+          <md-tooltip md-direction="left">Escritório</md-tooltip>
           <md-icon>location_city</md-icon>
         </md-button>
       </md-speed-dial-content>
